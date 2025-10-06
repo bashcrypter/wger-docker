ecb23dfa444a29cc7e3475acacd45f530470cf27
bashcrypter <217852049+bashcrypter@users.noreply.github.com>
2025-10-05 22:18:29 -0500

    add settings copy to ini

--- dev/init.sh
+++ dev/init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+. ./.env
+cp $WGER_CODEPATH/extras/docker/production/settings.py $WGER_CODEPATH/
 docker compose exec web wger bootstrap
 # docker compose exec web -- python3 manage.py sync-exercises
 # docker compose exec web -- wger load-online-fixtures
